adbc79219fe63d0d131e595bf87cda5a88b79e8d
Sachin Bhawar <sachinbhavar@gmail.com>
2025-05-04 16:30:13 +0530

    style updated

--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -57,9 +57,9 @@ function HomePage() {
                 />
             </form>
 
-            <div className="container-fluid mx-2 d-flex flex-column flex-md-row align-items-start">
+            <div className="container-fluid mx-auto d-flex flex-column align-items-center flex-sm-row align-items-sm-start">
                 {/* Sidebar - Takes up 3 columns on md and lg screens */}
-                <div className="col-md-3 col-lg-3 d-flex align-items-start justify-content-center">
+                <div className="d-flex align-items-start justify-content-center">
                     <FilterSidebar
                         setPriceRange={setPriceRange}
                         setCategories={setCategories}
@@ -68,7 +68,8 @@ function HomePage() {
                 </div>
 
                 {/* Product List - Takes remaining space */}
-                <div className="col-md-9 col-lg-9 d-flex align-items-start">
+                <div className="d-flex align-items-center">
+                    {/* <div className="col-md-9 col-lg-9 d-flex align-items-center"> */}
                     {products.length ? <ProductList products={filteredProducts} /> : null}
                 </div>
             </div>
